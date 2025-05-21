743791922f508e89a95c6088d57ad91599e95c79
Yullc <apanan0910@gmail.com>
2025-05-21 22:09:42 +0900

    프로젝트 스타트트라이앵글

---
+++ src/main/java/org/example/pcw_project/PcwProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.pcw_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcwProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcwProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pcw_project/PcwProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pcw_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcwProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
